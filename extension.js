9967c1b255524a6de1ab3f4f6309fc8c68ff47a0
pavan gouda <npavangouda@gmail.com>
2025-01-11 15:38:33 +0530

    fixed issue for double quotes

--- extension.js
+++ extension.js
@@ -1,8 +1,7 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 const vscode = require('vscode');
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const { decode } = require('he');
+const parseJson = require('json-parse-even-better-errors');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -11,60 +10,104 @@ async function activate(context) {
 
 	console.log('Extension "jsonCraft" is now active!');
 
-	const disposable = vscode.commands.registerCommand('jsonCraft.formatJson', async function () {
-		const editor = vscode.window.activeTextEditor;
+	const cleanTextUsingPatterns = (inputString) => {
+		let cleanedText = inputString;
 
-		if (!editor) {
-			vscode.window.showErrorMessage('No active editor found.');
-			return;
+		// Replace double quotes from the start and end of the text
+		if (cleanedText.startsWith('"') && cleanedText.endsWith('"')) {
+			cleanedText = cleanedText.slice(1, -1);
+		}
+		// Normalize escaped backslashes (\\ -> \)
+		cleanedText = cleanedText
+			.replace(/\\\\/g, '\\') // Normalize escaped backslashes (\\ -> \)
+			.replace(/\\/g, '') // Normalize escaped backslashes (\ -> '')
+			.replace(/"body":"\{"/g, '"body": {"') // Replace "body":"{" with "body": {"
+			.replace(/}"}/g, '}}').replace(/}"]/, '}]'); // Replace "}"}" with "}}"
+		if (cleanedText.indexOf(';for="') > 0) {
+			cleanedText = cleanedText.replace(/;for="/g, ';for=\\"');
+		}
+		if (cleanedText.indexOf('""') > 0) {	
+			cleanedText = cleanedText
+				.replace(/:""/g, ':"\\"')
+				.replace(/"",/g, '\\"",')
+				.replace(/""}/g, '\\""}');
+			// cleanedText = cleanedText.replace(/"([^"]*?)"/g, (match, group) => {
+			// 	const escapedValue = group.replace(/"/g, '\\"'); // Escape unescaped double quotes
+			// 	return escapedValue;
+			// });
 		}
 
-		// check if any selection is made
-		const selection = editor.selection;
-		const text = selection.isEmpty ? editor.document.getText() : editor.document.getText(selection);
+	// cleanedText = cleanedText.replace(/^"|"$/g, '');
+	return cleanedText;
+};
 
-		if (!text) {
-			vscode.window.showInformationMessage('No text found in the editor.');
-			return
-		}
+const disposable = vscode.commands.registerCommand('jsonCraft.formatJson', async function () {
+	const editor = vscode.window.activeTextEditor;
 
+	if (!editor) {
+		vscode.window.showErrorMessage('No active editor found.');
+		return;
+	}
+
+	// check if any selection is made
+	const selection = editor.selection;
+	const text = selection.isEmpty ? editor.document.getText() : editor.document.getText(selection);
+
+	if (!text) {
+		vscode.window.showInformationMessage('No text found in the editor.');
+		return
+	}
+
+	try {
+
+		// set the language mode to JSON
+		await vscode.languages.setTextDocumentLanguage(editor.document, 'json');
+
+		// Remove unnecessary escape characters before HTML entities
+		// let cleanedText = text.replace(/\\(&#[0-9]+;)/g, '$1');
+
+
+		// Decode HTML entities using 'he' package
+		let decodedText = decode(text);
+		let cleanedText = cleanTextUsingPatterns(decodedText);
+		let finalJsonText;
+		let isParseError = false;
 		try {
-			// Dynamically import the 'parse-json' and 'he' packages
-			const parseJson = (await import('parse-json')).default;
-			const he = (await import('he')).default;
-
-			// Remove unnecessary escape characters before HTML entities
-			let cleanedText = text.replace(/\\(&#[0-9]+;)/g, '$1');
-
-			// Decode HTML entities using 'he' package
-			let decodedText = he.decode(cleanedText);
-
 			// Parse the decoded text as JSON
-			const formattedJson = parseJson(decodedText);
-
+			let formattedJson = parseJson(cleanedText);
 			// Format JSON with indentation
-			const prettyJson = JSON.stringify(formattedJson, null, 4);
-
-			// Apply changes to the editor
-			editor.edit((editBuilder) => {
-				if (selection.isEmpty) {
-					const fullRange = new vscode.Range(
-						editor.document.positionAt(0),
-						editor.document.positionAt(text.length)
-					);
-					editBuilder.replace(fullRange, prettyJson);
-				} else {
-					editBuilder.replace(selection, prettyJson);
-				}
-			});
+			finalJsonText = JSON.stringify(formattedJson, null, 4);
 		} catch (error) {
-			vscode.window.showErrorMessage(
-				`Failed to format JSON: ${error.message}`
-			);
+			isParseError = true;
+			finalJsonText = cleanedText;
+			vscode.window.showErrorMessage(`Failed to parse JSON: ${error.message}`);
 		}
-	});
 
-	context.subscriptions.push(disposable);
+
+		// Apply changes to the editor
+		editor.edit((editBuilder) => {
+			if (selection.isEmpty) {
+				const fullRange = new vscode.Range(
+					editor.document.positionAt(0),
+					editor.document.positionAt(text.length)
+				);
+				editBuilder.replace(fullRange, finalJsonText);
+			} else {
+				editBuilder.replace(selection, finalJsonText);
+			}
+		});
+
+		// Trigger Format Document
+		await vscode.commands.executeCommand('editor.action.formatDocument');
+
+	} catch (error) {
+		vscode.window.showErrorMessage(
+			`Failed to craft JSON: ${error.message}`
+		);
+	}
+});
+
+context.subscriptions.push(disposable);
 }
 
 function deactivate() { }
--- test/extension.test.js
+++ test/extension.test.js
@@ -1,12 +1,9 @@
 
-const assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-const vscode = require('vscode');
-const fs = require('fs');
-const path = require('path');
-const he = require('he');
+const { fail, deepStrictEqual, strictEqual } = require('assert');
+const { workspace, window, commands } = require('vscode');
+const { existsSync, readFileSync } = require('fs');
+const { join } = require('path');
+const { encode } = require('he');
 // const myExtension = require('../extension');
 
 suite('Extension Test Suite', () => {
@@ -17,11 +14,14 @@ suite('Extension Test Suite', () => {
 	}, {
 		fileName: 'sample2.json',
 		title: 'Test Medium size json'
+	}, {
+		fileName: 'sample3.json',
+		title: 'Test start-end with double quotes and internal json is still invalid'
 	}];
 
 	const generateInput = (data) => {
 		let encodedData = JSON.stringify(data, (key, value) =>
-			typeof value === 'string' ? he.encode(value) : value
+			typeof value === 'string' ? encode(value) : value
 		);
 		// Escape the encoded characters
 		let escapedData = encodedData.replace(/"/g, '\&#34;');
@@ -31,19 +31,19 @@ suite('Extension Test Suite', () => {
 
 	testScenarios.forEach((testScenario) => {
 		test(testScenario.title, async () => {
-			const sampleFilePath = path.join(__dirname, 'data', testScenario.fileName);
-			if (!fs.existsSync(sampleFilePath)) {
-				assert.fail(`File not found: ${sampleFilePath}`);
+			const sampleFilePath = join(__dirname, 'data', testScenario.fileName);
+			if (!existsSync(sampleFilePath)) {
+				fail(`File not found: ${sampleFilePath}`);
 			}
 
 
-			const sampleData = JSON.parse(fs.readFileSync(sampleFilePath, 'utf8'));
+			const sampleData = JSON.parse(readFileSync(sampleFilePath, 'utf8'));
 
 			let input = sampleData.input;
 			const expectedOutput = sampleData.output;
 
 			if (!input && !expectedOutput) {
-				assert.fail('Input and expected output are missing in the sample file');
+				fail('Input and expected output are missing in the sample file');
 			}
 
 			if (!input && expectedOutput) {
@@ -51,48 +51,51 @@ suite('Extension Test Suite', () => {
 				input = generateInput(expectedOutput);
 			}
 
-			const editor = await vscode.workspace.openTextDocument({ content: input, language: 'json' });
-			const document = await vscode.window.showTextDocument(editor);
-			await vscode.commands.executeCommand('editor.action.selectAll');
-			await vscode.commands.executeCommand('editor.action.triggerSuggest');
-			await vscode.commands.executeCommand('jsonCraft.formatJson');
+			const editor = await workspace.openTextDocument({ content: input, language: 'json' });
+			const document = await window.showTextDocument(editor);
+			await commands.executeCommand('editor.action.selectAll');
+			await commands.executeCommand('editor.action.triggerSuggest');
+			await commands.executeCommand('jsonCraft.formatJson');
 
 			// Wait for the UI to update
 			await new Promise(resolve => setTimeout(resolve, 100));
 
 			const formattedText = JSON.parse(document.document.getText());
 
-			assert.deepStrictEqual(formattedText, expectedOutput);
+			deepStrictEqual(formattedText, expectedOutput);
 
-			await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+			await commands.executeCommand('workbench.action.closeActiveEditor');
 		});
 	});
 
 	test('Test invalid JSON', async () => {
-		const input = '{ "name": "John", "age": 30, "city": "New York" ';
-		const editor = await vscode.workspace.openTextDocument({ content: input, language: 'json' });
-		const document = await vscode.window.showTextDocument(editor);
-		await vscode.commands.executeCommand('editor.action.selectAll');
-		await vscode.commands.executeCommand('editor.action.triggerSuggest');
-		await vscode.commands.executeCommand('jsonCraft.formatJson');
+		const input = `{
+    "name": "John",
+    "age": 30,
+    "city": "New York"`;
+		const editor = await workspace.openTextDocument({ content: input, language: 'json' });
+		const document = await window.showTextDocument(editor);
+		await commands.executeCommand('editor.action.selectAll');
+		await commands.executeCommand('editor.action.triggerSuggest');
+		await commands.executeCommand('jsonCraft.formatJson');
 
 		const finalContent = document.document.getText();
-		assert.strictEqual(finalContent, input, 'The document should remain unchanged.');
+		strictEqual(finalContent, input, 'The document should remain unchanged.');
 
-		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+		await commands.executeCommand('workbench.action.closeActiveEditor');
 	});
 
 	test('Test empty input', async () => {
-		const editor = await vscode.workspace.openTextDocument({ content: '', language: 'json' });
-		const document = await vscode.window.showTextDocument(editor);
-		await vscode.commands.executeCommand('editor.action.selectAll');
-		await vscode.commands.executeCommand('editor.action.triggerSuggest');
-		await vscode.commands.executeCommand('jsonCraft.formatJson');
+		const editor = await workspace.openTextDocument({ content: '', language: 'json' });
+		const document = await window.showTextDocument(editor);
+		await commands.executeCommand('editor.action.selectAll');
+		await commands.executeCommand('editor.action.triggerSuggest');
+		await commands.executeCommand('jsonCraft.formatJson');
 
 		const finalContent = document.document.getText();
-		assert.strictEqual(finalContent, '', 'The document should remain empty for empty input.');
+		strictEqual(finalContent, '', 'The document should remain empty for empty input.');
 
-		await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+		await commands.executeCommand('workbench.action.closeActiveEditor');
 	}
 	);
 
